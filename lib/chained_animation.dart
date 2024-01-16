3ead6acae907f9dbea24575cd1ff8c8128bd9bcd
Aditya Dwivedi <dwikaljit@gmail.com>
2024-01-16 12:14:36 +0530

    Created Boilerplate for chained_animation.dart

--- lib/chained_animation.dart
+++ lib/chained_animation.dart
@@ -11,7 +11,25 @@ class _ChainedAnimationState extends State<ChainedAnimation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                color: const Color(0xff0057b7),
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                color: const Color(0xffffd700),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
